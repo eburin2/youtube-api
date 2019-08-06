483f0b618c4a0734f06eb0e29ff1e6f7c1c98969
Elliot Burin <eburin2@gmail.com>
2019-08-05 19:29:53 -0700

    add semantic-ui grid system to the app component and implement default search term in the componentDidMount lifecycle hook

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -8,8 +8,8 @@ const VideoDetail = ({ video }) => {
   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
   return (
     <div>
-      <div className="ui embed">
-        <iframe src={videoSrc} />
+      <div key={video.id.videoId} className="ui embed">
+        <iframe alt={video.snippet.title} title="video player" src={videoSrc} />
       </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
